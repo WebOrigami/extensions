9e46885dbbf3a86b146e7ece94c944d2f573c836
Jan Miksovsky <jan@miksovsky.com>
2024-02-26 11:28:24 -0800

    Rough in EXIF data extraction

---
+++ exif/src/exif.js
@@ -0,0 +1,12 @@
+import exifParser from "exif-parser";
+
+// Given a Buffer containing image data, return the EXIF metadata.
+export default async function exif(buffer) {
+  const parser = exifParser.create(buffer);
+  parser.enableTagNames(true);
+  parser.enableSimpleValues(true);
+  const parsed = await parser.parse();
+  // Convert an exif-parser result to a plain object.
+  const plain = Object.assign({}, parsed);
+  return plain;
+}
