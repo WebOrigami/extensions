f7a821c321be02bd38a038f5d60be7e6cb654f3f
Jan Miksovsky <jan@miksovsky.com>
2024-03-05 11:22:35 -0800

    Rename gdrive -> auth, GoogleDriveGraph -> GoogleDriveTree. Wrap returned tree with FileLoadersTransform.

--- gdrive/src/GoogleDriveTree.js
+++ gdrive/src/GoogleDriveTree.js
@@ -8,7 +8,7 @@ const googleExtensions = {
 /**
  * @implements {import("@weborigami/async-tree").AsyncTree}
  */
-export default class GoogleDriveGraph {
+export default class GoogleDriveTree {
   constructor(auth, folderId) {
     this.auth = auth;
     this.service = google.drive({ version: "v3", auth });
@@ -30,7 +30,7 @@ export default class GoogleDriveGraph {
     const googleFileTypes = {
       "application/vnd.google-apps.spreadsheet": gsheet,
       "application/vnd.google-apps.folder": (auth, id) =>
-        new GoogleDriveGraph(auth, id),
+        new GoogleDriveTree(auth, id),
     };
     const loader = googleFileTypes[item.mimeType] || getGoogleDriveFile;
     const value = await loader(this.auth, item.id);
--- gdrive/src/auth.js
+++ gdrive/src/auth.js
@@ -1,14 +1,16 @@
 import { Tree } from "@weborigami/async-tree";
+import { FileLoadersTransform } from "@weborigami/language";
 import * as googleApis from "googleapis";
-import GoogleDriveGraph from "./GoogleDriveGraph.js";
+import GoogleDriveTree from "./GoogleDriveTree.js";
 
 const scopes = [
   "https://www.googleapis.com/auth/drive.readonly",
   "https://www.googleapis.com/auth/spreadsheets.readonly",
 ];
 
-export default async function gdrive(credentialsTreelike) {
+export default async function auth(credentialsTreelike) {
   const credentials = await Tree.plain(credentialsTreelike);
   const auth = new googleApis.google.auth.GoogleAuth({ credentials, scopes });
-  return (folderId) => new GoogleDriveGraph(auth, folderId);
+  return (folderId) =>
+    new (FileLoadersTransform(GoogleDriveTree))(auth, folderId);
 }
