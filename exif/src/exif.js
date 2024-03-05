d62ce81cca18422906e5479c57bb012118ec9e8d
Jan Miksovsky <jan@miksovsky.com>
2024-03-05 11:20:33 -0800

    More Exif

--- exif/src/exif.js
+++ exif/src/exif.js
@@ -8,5 +8,11 @@ export default async function exif(buffer) {
   const parsed = await parser.parse();
   // Convert an exif-parser result to a plain object.
   const plain = Object.assign({}, parsed);
+  if (typeof parsed.tags?.ModifyDate === "number") {
+    // exif-parser says it tries to convert dates to JavaScript Date objects,
+    // but it looks like that doesn't always work. If the date's a number,
+    // convert it to a Date object.
+    plain.tags.ModifyDate = new Date(parsed.tags.ModifyDate * 1000);
+  }
   return plain;
 }
