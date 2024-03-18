32b118bdcda9b1981210a0c64ae16de215197685
Jan Miksovsky <jan@miksovsky.com>
2024-03-18 10:32:38 -0700

    Add support for Handlebars partials (nested template calls)

---
+++ handlebars/test/hbs_handler.test.js
@@ -0,0 +1,28 @@
+import { ObjectTree } from "@weborigami/async-tree";
+import { Scope } from "@weborigami/language";
+import assert from "node:assert";
+import { describe, test } from "node:test";
+import hbsHandler from "../src/hbs_handler.js";
+
+describe("Handlebars hbs extension handler", () => {
+  test("returns a function that applies a Handlebars template", async () => {
+    const template = `Hello, {{ name }}!`;
+    const fn = await hbsHandler.unpack(template);
+    const tree = new ObjectTree({ name: "world" });
+    const result = await fn(tree);
+    assert.equal(result, "Hello, world!");
+  });
+
+  test("uses parent scope to resolve partials", async () => {
+    const parent = Scope.treeWithScope(
+      {},
+      {
+        ["bold.hbs"]: `<strong>{{this}}</strong>`,
+      }
+    );
+    const template = `Hello, {{#> bold}}{{ name }}{{/bold}}!`;
+    const fn = await hbsHandler.unpack(template, { parent });
+    const result = await fn("world");
+    assert.equal(result, "Hello, <strong>world</strong>!");
+  });
+});
