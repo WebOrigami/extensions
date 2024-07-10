9b33b61b1d1ee19a6932974882452491e65938ce
Jan Miksovsky <jan@miksovsky.com>
2024-07-10 09:09:59 -0700

    Update old code

--- gdrive/src/headerKeys.js
+++ gdrive/src/headerKeys.js
@@ -1,8 +1,7 @@
-import { GraphHelpers } from "@graphorigami/origami";
+import { Tree } from "@weborigami/async-tree";
 
-export default async function headerKeys(variant) {
-  const graph = await GraphHelpers.from(variant);
-  const table = await GraphHelpers.plain(graph);
+export default async function headerKeys(treelike) {
+  const table = await Tree.plain(treelike);
   const rows = table.slice();
   const header = rows.shift();
   const result = [];
