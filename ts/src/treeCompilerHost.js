59af66d53a0313b19b39506a838924cb011ca43e
Jan Miksovsky <jan@miksovsky.com>
2024-08-01 09:36:52 -0700

    Rough in ts package

---
+++ ts/src/treeCompilerHost.js
@@ -0,0 +1,89 @@
+import {
+  DeepObjectTree,
+  keysFromPath,
+  toString,
+  Tree,
+} from "@weborigami/async-tree";
+import ts from "typescript";
+
+/**
+ * Given a tree of JavaScript and TypeScript files, create a TypeScript compiler
+ * host suitable for passing to ts.createProgram().
+ *
+ * @typedef {import("@weborigami/async-tree").Treelike} Treelike
+ * @param {Treelike} treelike
+ */
+export default async function treeCompilerHost(treelike) {
+  const plain = await Tree.plain(treelike);
+  return new TreeCompilerHost(plain);
+}
+
+/**
+ * A TypeScript compiler host that reads and writes files from a plain object.
+ * This subclasses DeepObjectTree so that it can be used as an AsyncTree too.
+ *
+ * Note: TypeScript docs call the first argument to functions like getSourceFile
+ * and writeFile `fileName`, but it's actually a slash-separated path.
+ */
+class TreeCompilerHost extends DeepObjectTree {
+  fileExists(filePath) {
+    return traversePath(this.object, filePath) !== undefined;
+  }
+
+  getCanonicalFileName(fileName) {
+    return fileName;
+  }
+
+  getCurrentDirectory() {
+    return "/";
+  }
+
+  getDefaultLibFileName() {
+    return "";
+  }
+
+  getSourceFile(filePath) {
+    return ts.createSourceFile(
+      filePath,
+      toString(traversePath(this.object, filePath))
+    );
+  }
+
+  readFile(filePath) {
+    return toString(traversePath(this.object, filePath));
+  }
+
+  useCaseSensitiveFileNames() {
+    return true;
+  }
+
+  writeFile(filePath, contents) {
+    const keys = keysFromPath(filePath);
+    const fileName = keys.pop();
+    // Traverse to the appropriate parent object, creating any missing objects.
+    let current = this.object;
+    for (const key of keys) {
+      if (!current[key]) {
+        current[key] = {};
+      }
+      current = current[key];
+    }
+    current[fileName] = contents;
+  }
+}
+
+// Traverse the path in a plain object.
+function traversePath(plain, path) {
+  const keys = keysFromPath(path);
+  let current = plain;
+  for (const key of keys) {
+    if (!key) {
+      continue;
+    }
+    current = current[key];
+    if (!current) {
+      return undefined;
+    }
+  }
+  return current;
+}
---
+++ ts/test/treeCompilerHost.test.js
@@ -0,0 +1,30 @@
+import { Tree } from "@weborigami/async-tree";
+import assert from "node:assert";
+import { describe, test } from "node:test";
+import ts from "typescript";
+import treeCompilerHost from "../src/treeCompilerHost.js";
+
+describe("treeCompilerHost", () => {
+  test("can be fed to TypeScript compiler", async () => {
+    const tree = {
+      "index.ts": "console.log('Hello, world!');",
+    };
+    const host = await treeCompilerHost(tree);
+    const paths = await Tree.paths(tree);
+    const options = {
+      allowJs: true,
+      module: ts.ModuleKind.ESNext,
+    };
+    const program = ts.createProgram(paths, options, host);
+    program.emit();
+
+    const sourceFile = program.getSourceFile("index.ts");
+    assert(sourceFile);
+    const statements = sourceFile.statements;
+    assert.equal(statements.length, 1);
+    assert.equal(
+      statements[0].getText(sourceFile),
+      "console.log('Hello, world!');"
+    );
+  });
+});
