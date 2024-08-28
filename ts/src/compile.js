c8de79912de4c213eee91e40afeb6e4ba4d7d414
Jan Miksovsky <jan@miksovsky.com>
2024-08-28 16:08:30 -0700

    Simplify

--- ts/src/compile.js
+++ ts/src/compile.js
@@ -1,33 +1,32 @@
 import { isUnpackable, Tree } from "@weborigami/async-tree";
 import ts from "typescript";
-import treeCompilerHost from "./host.js";
+import host from "./host.js";
 
 export default async function compile(treelike, options) {
-  const host = await treeCompilerHost(treelike);
+  const treeHost = await host(treelike);
 
   if (!options) {
     // Read config from tree.
-    options = await host.get("tsconfig.json");
+    options = await treeHost.get("tsconfig.json");
     if (typeof options === "string") {
       options = JSON.parse(options);
     }
-  }
-
-  if (isUnpackable(options)) {
+  } else if (isUnpackable(options)) {
     // Options are packed as a file Buffer or similar structure; unpack and
     // parse as TypeScript compiler options.
     const unpacked = await options.unpack();
     const plain = await Tree.plain(unpacked);
-    // Note: despite the "Json" in the name, this TypeScript helper function
-    // actually parses compiler options from a plain object with string values.
-    // E.g., "ESNext" will be parsed as the `ESNext` enum value.
-    options = ts.convertCompilerOptionsFromJson(plain).options;
   } else if (options) {
     options = await Tree.plain(options);
   }
 
-  const paths = await Tree.paths(host);
-  const program = ts.createProgram(paths, options, host);
+  // Note: despite the "Json" in the name, this TypeScript helper function
+  // actually parses compiler options from a plain object with string values.
+  // E.g., "ESNext" will be parsed as the `ESNext` enum value.
+  options = ts.convertCompilerOptionsFromJson(options).options;
+
+  const paths = await Tree.paths(treeHost);
+  const program = ts.createProgram(paths, options, treeHost);
   program.emit();
-  return host;
+  return treeHost;
 }
