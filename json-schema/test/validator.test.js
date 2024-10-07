007d5762e664f7ba69c785f9785c87690adb8eb4
Jan Miksovsky <jan@miksovsky.com>
2024-10-07 12:10:50 -0700

    Test validating file system folder

--- json-schema/test/validator.test.js
+++ json-schema/test/validator.test.js
@@ -1,6 +1,12 @@
+import { FileTree } from "@weborigami/async-tree";
 import assert from "node:assert";
+import path from "node:path";
 import { describe, test } from "node:test";
+import { fileURLToPath } from "node:url";
 import validator from "../src/validator.js";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const fixtures = new FileTree(path.join(dirname, "fixtures"));
 
 describe("validator", () => {
   test("returns input data as is if it is valid", async () => {
@@ -33,6 +39,27 @@ describe("validator", () => {
       () => validate(input, "foo.json"),
       new Error(
         "Validation failed:\nfoo.json: must have required property 'name'"
+      )
+    );
+  });
+
+  test("can validate file system folder", async () => {
+    const markdown = await fixtures.get("markdown");
+    const schema = {
+      type: "object",
+      patternProperties: {
+        // File names should end in .md
+        "\\.md$": {
+          type: "object", // File values will be Buffer objects
+        },
+      },
+      additionalProperties: false, // Don't allow other file names
+    };
+    const validate = await validator(schema);
+    await assert.rejects(
+      () => validate(markdown, "markdown"),
+      new Error(
+        "Validation failed:\nmarkdown: must NOT have additional properties"
       )
     );
   });
