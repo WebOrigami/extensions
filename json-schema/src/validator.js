32faa5eb4f8c1d2f8bb4626675434d4368f7df5e
Jan Miksovsky <jan@miksovsky.com>
2025-02-28 18:04:07 -0800

    More error detail

--- json-schema/src/validator.js
+++ json-schema/src/validator.js
@@ -14,10 +14,40 @@ export default async function validator(treelike) {
         let message = key ? `${key}: ` : "";
         message += error.instancePath ? `${error.instancePath}: ` : "";
         message += error.message;
+        if (error.params.additionalProperty) {
+          message += ` (${error.params.additionalProperty})`;
+        }
+        message += error.instancePath
+          ? ": " + formatInstance(getInstance(data, error.instancePath))
+          : "";
         return message;
       });
-      throw new Error("Validation failed:\n" + messages.join("\n"));
+      throw new Error("Validation failed:\n" + messages.join("\n\n"));
     }
     return data;
   };
+}
+
+function formatInstance(object) {
+  let text =
+    typeof object === "object" ? JSON.stringify(object) : object.toString();
+  if (text.length > 255) {
+    text = text.slice(0, 255) + "…";
+  }
+  return text;
+}
+
+function getInstance(data, instancePath) {
+  const path = instancePath.split("/");
+  let current = data;
+  for (let i = 1; i < path.length; i++) {
+    const key = path[i];
+    if (key === "") continue;
+    if (key in current) {
+      current = current[key];
+    } else {
+      return undefined;
+    }
+  }
+  return current;
 }
