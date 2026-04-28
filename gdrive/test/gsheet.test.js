b4f9523145eb9b57c401d92159837cd301855e58
Jan Miksovsky <jan@miksovsky.com>
2026-04-28 16:54:17 -0700

    Track changes

--- gdrive/test/gsheet.test.js
+++ gdrive/test/gsheet.test.js
@@ -1,11 +1,14 @@
+import { FileMap } from "@weborigami/async-tree";
 import assert from "node:assert";
 import { promises as fs } from "node:fs";
 import { describe, test } from "node:test";
 import auth from "../src/auth.js";
 
+const parentUrl = new URL("..", import.meta.url);
+const parent = new FileMap(parentUrl);
 const credsPath = new URL("../creds.json", import.meta.url);
 const creds = JSON.parse(await fs.readFile(credsPath));
-const folderFn = await auth(creds);
+const folderFn = await auth(creds, { parent });
 const fixture = folderFn("1X3MWPXwwYXWarhNiCBIxCvGTyiBqISAF");
 
 describe("gsheet", () => {
