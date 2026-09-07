4d153ca6cc3846c5ed4340993904da355330582f
Jan Miksovsky <jan@miksovsky.com>
2026-09-07 16:28:14 -0700

    Fold publishing into NetlifyMap as replaceWith

--- netlify/src/NetlifyMap.js
+++ netlify/src/NetlifyMap.js
@@ -1,16 +1,19 @@
 import {
   AsyncMap,
   naturalOrder,
+  pack,
   setParent,
   trailingSlash,
   Tree,
 } from "@weborigami/async-tree";
 import { fetchWithBackoff } from "@weborigami/origami";
+import mapLimit from "./mapLimit.js";
 
+/**
+ * An AsyncMap representation of a Netlify site.
+ */
 export default class NetlifyMap extends AsyncMap {
   /**
-   * An AsyncMap representation of a Netlify site
-   *
    * @param {{ path?: string, projectId: string, projectName: string, token: string|Uint8Array }} options
    */
   constructor(options) {
@@ -101,7 +104,97 @@ export default class NetlifyMap extends AsyncMap {
     return result;
   }
 
-  // replaceWith(source) {}
+  async replaceWith(source) {
+    if (this.path) {
+      throw new Error(
+        "Netlify: You can only call replaceWith() on the project's root.",
+      );
+    }
+
+    // What files are in the source tree?
+    const manifest = await Tree.manifest(source);
+
+    // Netlify wants leading slashes in the manifest paths.
+    const deflated = await Tree.deflatePaths(manifest, { base: "/" });
+    const files = await Tree.plain(deflated);
+
+    const body = JSON.stringify({ files });
+    const response = await fetch(
+      `https://api.netlify.com/api/v1/sites/${this.projectId}/deploys`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.token}`,
+        },
+        body,
+      },
+    );
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `Netlify API request failed with status ${response.status}: ${errorText}`,
+      );
+    }
+
+    // See what files Netlify wants.
+    const data = await response.json();
+    const { id: deployId, required, ssl_url } = data;
+    if (required.length === 0) {
+      console.log(`Site is up to date: ${ssl_url}`);
+      return;
+    }
+
+    // Netlify gives us a set of hashes. For each one, find the first path in our
+    // tree that has that hash.
+    const uploadPaths = required.map((hash) => {
+      const path = Object.keys(files).find((path) => files[path] === hash);
+      if (!path) {
+        throw new Error(
+          `Netlify requested a file with a hash that wasn't in the provided source files: ${hash}`,
+        );
+      }
+      return path;
+    });
+
+    // Upload each required file.
+    const deployUrl = `https://api.netlify.com/api/v1/deploys/${deployId}/files`;
+    await mapLimit(
+      uploadPaths,
+      (path) => uploadFile(source, path, deployUrl, this.token),
+      8,
+    );
+
+    console.log(`Uploaded ${uploadPaths.length} file(s) to ${ssl_url}`);
+  }
 
   trailingSlashKeys = true;
+}
+
+// Upload the file at the given path to Netlify
+async function uploadFile(site, path, deployUrl, token) {
+  const value = await Tree.traversePath(site, path);
+  let body;
+  try {
+    body = pack(value);
+  } catch (err) {
+    throw new Error(`Netlify: Can't convert to buffer: ${path}`);
+  }
+  const encodedPath = path.split("/").map(encodeURIComponent).join("/");
+  const uploadUrl = `${deployUrl}/${encodedPath}`;
+  const uploadResponse = await fetchWithBackoff(uploadUrl, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/octet-stream",
+      Authorization: `Bearer ${token}`,
+    },
+    body,
+  });
+  if (!uploadResponse.ok) {
+    const errorText = await uploadResponse.text();
+    throw new Error(
+      `Netlify: Failed to upload ${path}: ${uploadResponse.status} ${errorText}`,
+    );
+  }
 }
