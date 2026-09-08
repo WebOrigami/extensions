33c6f88647d18834e092770225477ce8e1003033
Jan Miksovsky <jan@miksovsky.com>
2026-09-08 08:41:18 -0700

    NetlifyMap tests

---
+++ netlify/test/NetlifyMap.test.js
@@ -0,0 +1,29 @@
+import { Tree } from "@weborigami/async-tree";
+import assert from "node:assert";
+import fs from "node:fs/promises";
+import { describe, test } from "node:test";
+import auth from "../src/auth.js";
+
+const tokenPath = new URL("../token.txt", import.meta.url);
+const tokenBuffer = await fs.readFile(tokenPath);
+const token = new TextDecoder().decode(tokenBuffer).trim();
+
+const fixture = await auth({
+  projectId: "59bca707-6a99-44b0-827d-024ba7d2ec73",
+  projectName: "origami-netlify-test",
+  token,
+});
+
+describe("NetlifyMap", () => {
+  test("get", async () => {
+    const buffer = await fixture.get("index.html");
+    const text = new TextDecoder().decode(buffer);
+    assert(text.includes("<!DOCTYPE html>"));
+  });
+
+  test("keys", async () => {
+    const keys = await Tree.keys(fixture);
+    assert(keys.includes("assets/"));
+    assert(keys.includes("index.html"));
+  });
+});
